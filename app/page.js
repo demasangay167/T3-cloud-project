53909e89fa125351efa8f1439a2a571fb4057207
Sangay <sdema6537@gmail.com>
2024-02-27 16:37:06 +0600

    add new image

--- app/page.js
+++ app/page.js
@@ -1,35 +1,48 @@
-import Quote from "./components/quote"
-import Lyrics from "./components/lyrics"
+"use client"
 
-async function getData() {
-	const res = await fetch("https://pixabay.com/api/?key="+ process.env.API_KEY +"&q=yellow+flowers&image_type=photo&pretty=true")
+import { useState } from "react"
 
-	if (!res.ok) {
-		throw new Error('Failed to fetch data')
-	}
+export default function Home() {
+	const [quotes, setQuotes] = useState([])
+	const [text, setText] = useState("Kuzuzangpo la")
 
-	return res.json()
-}
-
-export default async function Home() {
-	const data = await getData()
-	console.log(data)
 	return (
-		<>
+		<div className="mx-auto w-fit mt-10">
+			<div className="text-4xl text-red-500 text-start align-top ps-0">
+				{quotes.map((quote) => {
+					return (
+						<p class>
+							{quote}
+						</p>
+					)
+				})}
+			</div>
+			<div className="text-4xl text-purple-900 text-center mt-12 items-start font-bold">
+				{text}
 			
-		</>
-	)
-}
+			</div>
+			<input
+				className="text-2xl text-black mt-10 rounded-xl text-indigo-600 font-bold shadow-2xl"
+				type="text"
+				onChange={(event) => setText(event.target.value)}
+				placeholder="Enter Text Here"
+			/>
+			<button
+				className="text-2xl ml-8 font-bold border-4 px-2 ring-offset-black ring-offset-8s rounded-xl bg-violet-500 hover:bg-violet-600 shadow-2xl"
+				onClick={() => {
+					const newQuotes = quotes.concat([text])
+					setQuotes(newQuotes)
+					setText("")
 
-import Image from 'next/image'
- 
-export default function Page() {
-  return (
-    <Image
-      src="/profile.png"
-      width={500}
-      height={500}
-      alt="Picture of the author"
-    />
-  )
+
+
+				}}
+			>
+				Save Text
+			</button>
+			<button>Delete text</button>
+			className="text-2xl ml-8 font-bold border-4 px-2 ring-offset-black ring-offset-8s rounded-xl bg-violet-500 hover:bg-violet-600 shadow-2xl"
+				onClick={() => {
+		</div>
+	)
 }
